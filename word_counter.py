89f463d2b3f95b9f5ab2622ec42409a05bc68c19
Manpreet Bhatti <manpreet@bhatti.net>
2019-05-15 03:24:29 -0400

    Update word_counter.py
    Reverted back to old functions algorithm with the addition of sets. Having a class wasn't necessary and messed with the data, created a bug which counted more words than it should've.

--- word_counter.py
+++ word_counter.py
@@ -1,39 +1,35 @@
 import sys # Importing the ability to use the command line to input text files
 import string # Imported to use the punctuation feature
 
-class countWords:
-    def __init__(self, file_new):
-        self.file_new = file_new
+def count_the_words(file_new): # Counts the total amount of words
+    bunch_of_words = file_new.split(" ")
+    amount_of_words = len(bunch_of_words)
+    return amount_of_words
 
-    def count_the_words(self): # Counts the total amount of words
-        bunch_of_words = self.file_new.split(" ")
-        amount_of_words = len(bunch_of_words)
-        return amount_of_words
+def most_common(file_new): # Counts and prints the most common words in (word, count) format
+    for p in string.punctuation:  # Cleans the punctuation
+        file_new = file_new.replace(p, " ")
 
-    def most_common(self): # Counts and prints the most common words in (word, count) format
-        for p in string.punctuation:  # Cleans the punctuation
-            self.file_new = self.file_new.replace(p, " ")
-
-        new_words = self.file_new.lower().split()
-        
-        lone = set()  # Set of unique words
+    new_words = file_new.lower().split()
+    
+    lone = set()  # Set of unique words
+    for w in new_words:
+        lone.add(w)
+    
+    pairs = []  # List of (count, unique) tuples
+    for l in lone:
+        count = 0
         for w in new_words:
-            lone.add(w)
-        
-        pairs = []  # List of (count, unique) tuples
-        for l in lone:
-            count = 0
-            for w in new_words:
-                if w == l:
-                    count += 1
-            pairs.append((count, l))
-        
-        pairs.sort()  # Sort the list
-        pairs.reverse()  # Reverse it, making highest count first
-        
-        for i in range(min(10, len(pairs))):  # Print the ten most frequent words
-            count, word = pairs[i]
-            print("%s: %d" %(word, count))
+            if w == l:
+                count += 1
+        pairs.append((count, l))
+    
+    pairs.sort()  # Sort the list
+    pairs.reverse()  # Reverse it, making highest count first
+    
+    for i in range(min(10, len(pairs))):  # Print the ten most frequent words
+        count, word = pairs[i]
+        print("%s: %d" %(word, count))
 
 if __name__ == "__main__": # Run code below if a text file is inputted
     if len(sys.argv) < 2:
@@ -43,12 +39,10 @@ if __name__ == "__main__": # Run code below if a text file is inputted
 filename = sys.argv[1]
 
 f = open(filename, "r")
-file_read = f.read()
+file_data = f.read()
 f.close()
 
-file_data = countWords(file_read)
+most_common(file_data)
 
-file_data.most_common()
-
-num_of_words = file_data.count_the_words()
+num_of_words = count_the_words(file_data)
 print("The total number of words are: %d" %(num_of_words))
